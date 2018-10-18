98e8b6a6a2f73662cb6e9978f385332ae3c84fa1
ryanweaver718 <ryan.weaver718@gmail.com>
2018-10-17 20:05:39 -0400

    done

--- PythonProject/urls.py
+++ PythonProject/urls.py
@@ -20,9 +20,9 @@ from django.conf.urls.static import static
 import jobs.views
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', jobs.views.home, name='home'),
-                  path('blog/', include('blog.urls')),
-                  path('jobscanner/', include('jobscanner.urls')),
-                  path('accounts/', include('django.contrib.auth.urls')),
+	              path('admin/', admin.site.urls),
+	              path('', jobs.views.home, name='home'),
+	              path('blog/', include('blog.urls')),
+	              path('jobscanner/', include('jobscanner.urls')),
+	              path('accounts/', include('django.contrib.auth.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jobscanner/Nlp.py
+++ jobscanner/Nlp.py
@@ -18,113 +18,113 @@ from collections import Counter
 
 class Nlp:
 
-    @staticmethod
-    def central(text):
-        text = Nlp.remove_symbols(text)
-        text = Nlp.remove_stops(text)
-        text = Nlp.remove_unneeded_words(text)
-        text = Nlp.clean_up(text)
-        return text
+	@staticmethod
+	def central(text):
+		text = Nlp.remove_symbols(text)
+		text = Nlp.remove_stops(text)
+		text = Nlp.remove_unneeded_words(text)
+		text = Nlp.clean_up(text)
+		return text
 
-    @staticmethod
-    def remove_symbols(text):
-        new_text = text.lower()
-        new_text = word_tokenize(new_text)
-        characters = r'!\"$%\'()*,”“.-/;:–+<>=@[\]`’~^_{}•&|?'
-        for character in characters:
-            while character in new_text:
-                new_text.remove(character)
-        text = ' '.join(new_text)
-        while '/' in text:
-            text = text.replace('/', ' ')
-        while '·' in text:
-            text = text.replace('·', '')
+	@staticmethod
+	def remove_symbols(text):
+		new_text = text.lower()
+		new_text = word_tokenize(new_text)
+		characters = r'!\"$%\'()*,”“.-/;:–+<>=@[\]`’~^_{}•&|?'
+		for character in characters:
+			while character in new_text:
+				new_text.remove(character)
+		text = ' '.join(new_text)
+		while '/' in text:
+			text = text.replace('/', ' ')
+		while '·' in text:
+			text = text.replace('·', '')
 
-        return text
+		return text
 
-    @staticmethod
-    def remove_stops(text):
-        stop_words = set(stopwords.words('english'))
-        word_tokens = word_tokenize(text)
-        filtered_sentence = []
-        for w in word_tokens:
-            if w not in stop_words:
-                filtered_sentence.append(w)
-        filtered_sentence = ' '.join(filtered_sentence).lower()
-        return filtered_sentence
+	@staticmethod
+	def remove_stops(text):
+		stop_words = set(stopwords.words('english'))
+		word_tokens = word_tokenize(text)
+		filtered_sentence = []
+		for w in word_tokens:
+			if w not in stop_words:
+				filtered_sentence.append(w)
+		filtered_sentence = ' '.join(filtered_sentence).lower()
+		return filtered_sentence
 
-    @staticmethod
-    def remove_unneeded_words(text):
-        remove_words = []
-        # open the remove words and get them into an trimmed string that will be made into a list
-        path = os.getcwd()
-        try:
-            # Windows
-            # with open(path+"\jobscanner\Remove.txt", "r") as f:
-            # Mac
-            with open(path+"/jobscanner/Remove.txt", "r") as f:
-                for line in f:
-                    remove_words.append(line)
-        except IOError as exc:
-            if exc.errno != errno.EISDIR:
-                raise
+	@staticmethod
+	def remove_unneeded_words(text):
+		remove_words = []
+		# open the remove words and get them into an trimmed string that will be made into a list
+		path = os.getcwd()
+		try:
+			# Windows
+			# with open(path+"\jobscanner\Remove.txt", "r") as f:
+			# Mac
+			with open(path + "/jobscanner/Remove.txt", "r") as f:
+				for line in f:
+					remove_words.append(line)
+		except IOError as exc:
+			if exc.errno != errno.EISDIR:
+				raise
 
-        all_remove_words = ' '.join(remove_words)
-        while '  ' in all_remove_words:
-            all_remove_words = all_remove_words.replace('  ', ' ')
+		all_remove_words = ' '.join(remove_words)
+		while '  ' in all_remove_words:
+			all_remove_words = all_remove_words.replace('  ', ' ')
 
-        token_remove_words = word_tokenize(all_remove_words)
-        token_remove_words.sort()
+		token_remove_words = word_tokenize(all_remove_words)
+		token_remove_words.sort()
 
-        try:
-            # Windows
-            # with open(path+"\jobscanner\RemoveOutput.txt", "w+") as f:
-            # Mac
-            with open(path+"/jobscanner/RemoveOutput.txt", "w+") as f:
-                for word in token_remove_words:
-                    f.write(word + "\n")
-        except IOError as exc:
-            if exc.errno != errno.EISDIR:
-                raise
+		try:
+			# Windows
+			# with open(path+"\jobscanner\RemoveOutput.txt", "w+") as f:
+			# Mac
+			with open(path + "/jobscanner/RemoveOutput.txt", "w+") as f:
+				for word in token_remove_words:
+					f.write(word + "\n")
+		except IOError as exc:
+			if exc.errno != errno.EISDIR:
+				raise
 
-        word_tokens = word_tokenize(text)
-        filtered_sentence = []
-        for w in word_tokens:
-            if w not in token_remove_words:
-                filtered_sentence.append(w)
-        filtered_sentence = ' '.join(filtered_sentence).lower()
+		word_tokens = word_tokenize(text)
+		filtered_sentence = []
+		for w in word_tokens:
+			if w not in token_remove_words:
+				filtered_sentence.append(w)
+		filtered_sentence = ' '.join(filtered_sentence).lower()
 
-        return filtered_sentence
+		return filtered_sentence
 
-    @staticmethod
-    def clean_up(text):
-        # loop to remove double spaces and trim string
-        while True:
-            if '  ' in text:
-                text = re.sub(r'  ', r' ', text)
-            else:
-                break
-        while ' \'s' in text:
-            text = text.replace('\'s', '')
-        text = text.strip()
-        return text
+	@staticmethod
+	def clean_up(text):
+		# loop to remove double spaces and trim string
+		while True:
+			if '  ' in text:
+				text = re.sub(r'  ', r' ', text)
+			else:
+				break
+		while ' \'s' in text:
+			text = text.replace('\'s', '')
+		text = text.strip()
+		return text
 
-    @staticmethod
-    def return_dictionary(text):
-        reg_word_list = word_tokenize(text)
-        word_count_dict = Counter(words for words in reg_word_list)
-        return word_count_dict
+	@staticmethod
+	def return_dictionary(text):
+		reg_word_list = word_tokenize(text)
+		word_count_dict = Counter(words for words in reg_word_list)
+		return word_count_dict
 
-    @staticmethod
-    def return_lemma_dictionary(text):
-        lm = WordNetLemmatizer()
-        ls = LancasterStemmer()
-        lemma_list = []
-        word_list = word_tokenize(text)
-        for word in word_list:
-            lemma_word = lm.lemmatize(word)
-            stemmed = ls.stem(lemma_word)
-            lemma_list.append(stemmed)
-        lemma_count_dict = Counter(words for words in lemma_list)
+	@staticmethod
+	def return_lemma_dictionary(text):
+		lm = WordNetLemmatizer()
+		ls = LancasterStemmer()
+		lemma_list = []
+		word_list = word_tokenize(text)
+		for word in word_list:
+			lemma_word = lm.lemmatize(word)
+			stemmed = ls.stem(lemma_word)
+			lemma_list.append(stemmed)
+		lemma_count_dict = Counter(words for words in lemma_list)
 
-        return lemma_count_dict
+		return lemma_count_dict
